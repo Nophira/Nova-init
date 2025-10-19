a94a42eb1def626497987e49cd485676fe3da64c
Snyxex <153672410+Snyxex@users.noreply.github.com>
2025-10-19 19:55:28 +0200

    chore(release): 2.1.19

--- apps/nova-init/tsdown.config.ts
+++ apps/nova-init/tsdown.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
     entry: ['./src/index.ts'],
+    format: 'esm',
+    clean: true,
     platform: 'node',
+    outputOptions:{
+        banner: '#!/usr/bin/env node',
+    }
 });
